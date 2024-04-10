2d0613fc87d5889630c4748f8cb8ae8c1149ef5c
Rodrigo Bighetti <robighetti@gmail.com>
2024-04-10 07:10:45 -0300

    adicionado a config do vitest, supertest e testes e2e de authenticate e create-account

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,18 +1,36 @@
-import { PrismaClient } from '@prisma/client'
 import 'dotenv/config'
+
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 
 const prisma = new PrismaClient()
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set')
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
 }
 
-function generateUniqueDatabaseUrl(schemaId: string) {}
+const schemaId = randomUUID()
 
 beforeAll(async () => {
-  await prisma.$connect()
+  const databaseURL = generateUniqueDatabaseURL(schemaId)
+
+  console.log(databaseURL)
+
+  process.env.DATABASE_URL = databaseURL
+
+  execSync('pnpm prisma migrate deploy')
 })
 
 afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
   await prisma.$disconnect()
 })
